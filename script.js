39f3932da7149db53c8d8948e3ffd0c59434ea60
MDABUSAYED <saeed.robi@gmail.com>
2018-03-05 11:56:19 +0600

    added new java_script file

---
+++ script.js
@@ -0,0 +1,12 @@
+var numbers = [1,2,3,4,5]
+
+function sum (n){
+	var summation = 0;
+	
+	// do the actual sum
+	
+	sumation = n.reduce(function (o,n){ return o+n ;});
+	return sumation;
+}
+
+console.log( sum( numbers ) );
